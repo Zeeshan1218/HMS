08dd801f0b8e2754fce4c877de5f2073926365ae
Zeeshan Farid <zeeshanfarid4@gmail.com>
2025-11-19 22:15:18 +0500

    testing_endpoints

--- exercises/app.py
+++ exercises/app.py
@@ -3,6 +3,9 @@ app = Flask (__name__)
 @app.route("/")
 def home():
     return "Hello, Flask is working!"
+@app.route("/calculated")
+def home1():
+    return "Hello, Flask is not working!"
 
 if __name__ == "__main__":
     app.run(debug=True)
